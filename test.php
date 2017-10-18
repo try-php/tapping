79797f47d7bda1a489560de5afb00a414824fa12
Willi E <troublete@users.noreply.github.com>
2017-10-19 00:07:06 +0200

    Feature/flag support (#1)
    * (lib) added flag support
    
    * (pkg) fixed argument interpretation
    
    * (clean) removed demo
    
    * (docs) updated changelog

--- test.php
+++ test.php
@@ -6,6 +6,10 @@ use function Crayon\{text};
 use function Load\{dots,removeLastCharsByCount};
 
 function test(string $desc, callable $test) {
+	// build flag
+	$options = getopt('b', ['build', 'ci']);
+	$buildState = isset($options['b']) || isset($options['build']) || isset($options['ci']);
+
 	$loadingDesc = text($desc)->yellow();
 	$successIndicator = text('ok')->green();
 	$successMessage = "$desc $successIndicator";
@@ -28,7 +32,10 @@ function test(string $desc, callable $test) {
 				return "$loadingDesc";
 			}, "$successMessage");
 		} catch (\Exception $ex) {
-			echo "$failMessage\n";
+			fwrite(fopen('php://output', 'w'), "$failMessage\n");
+			if ($buildState) {
+				exit(1);
+			}
 		}
 	} else {
 		exit;
