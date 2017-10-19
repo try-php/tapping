1102c8e55d9ea55e37a7521c92acf7660708e065
Willi Eßer <willi.esser@troublete.com>
2017-10-19 21:03:59 +0200

    (examples) added example for exception fail

--- examples/test.php
+++ examples/test.php
@@ -3,12 +3,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use function Tapping\{test, todo};
 
-test('some test that gonna fail', function () {
-	exit(1);
+test('some test that gonna pass', function () {
+	exit;
 });
 
-test('some test that gonna pass', function () {
-	exit(0);
+test('some test that gonna fail with message', function () {
+	throw new Exception('Some very serious error');
+});
+
+
+test('some test that just fails', function () {
+	exit(1);
 });
 
 todo('some test i need to write');
