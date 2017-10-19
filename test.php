80ef3254c683c4734dbfc65991ba7799ff39061b
Willi Eßer <willi.esser@troublete.com>
2017-10-19 14:37:40 +0200

    (lib) added todo function, fixed naming

--- test.php
+++ test.php
@@ -5,21 +5,29 @@ use function Task\{forkTask, getProcessStatus};
 use function Crayon\{text};
 use function Load\{dots,removeLastCharsByCount};
 
-function test(string $desc, callable $test) {
+/**
+ * function to trigger a atomic test run and show an success/fail indicator (and on provided flag exit the parent process)
+ * @param string $description
+ * @param callable $test
+ */
+function test(string $description, callable $test) {
 	// build flag
 	$options = getopt('b', ['build', 'ci']);
-	$buildState = isset($options['b']) || isset($options['build']) || isset($options['ci']);
+	$buildFlag = isset($options['b']) || isset($options['build']) || isset($options['ci']);
 
-	$loadingDesc = text($desc)->yellow();
+	$loadingDescription = text($description)->yellow();
+
 	$successIndicator = text('ok')->green();
-	$successMessage = "$desc $successIndicator";
+	$successMessage = "$description $successIndicator";
+
 	$failIndicator = text('not ok')->red();
-	$failMessage = "$desc $failIndicator";
+	$failMessage = "$description $failIndicator";
 
 	$pid = forkTask($test, []);
+
 	if ($pid > 0) {
 		try {
-			dots(function () use ($pid, $loadingDesc) {
+			dots(function () use ($pid, $loadingDescription) {
 				$processStatus = getProcessStatus($pid, $status);
 				if ($processStatus > 0) {
 					if ($status === 0) {
@@ -29,15 +37,25 @@ function test(string $desc, callable $test) {
 					}
 				}
 
-				return "$loadingDesc";
+				return "$loadingDescription";
 			}, "$successMessage");
 		} catch (\Exception $ex) {
 			fwrite(fopen('php://output', 'w'), "$failMessage\n");
-			if ($buildState) {
+			if ($buildFlag) {
 				exit(1);
 			}
 		}
 	} else {
 		exit;
 	}
+}
+
+/**
+ * function to trigger an TODO indicator on test run
+ * @param string $description
+ */
+function todo(string $description) {
+	$todoIndicator = text('todo')->magenta()->italic();
+	$todoDescription = text($description);
+	fwrite(fopen('php://output', 'w'), "$todoIndicator $todoDescription\n");
 }
